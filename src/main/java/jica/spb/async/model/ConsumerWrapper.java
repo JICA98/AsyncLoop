1046c0478fb3b65ed5fc8cfcd2df4c33496736b9
jica <jica.etzc@gmail.com>
2023-08-13 17:02:18 +0530

    javadoc for more

--- src/main/java/jica/spb/async/model/ConsumerWrapper.java
+++ src/main/java/jica/spb/async/model/ConsumerWrapper.java
@@ -7,6 +7,11 @@ import lombok.Value;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
+/**
+ * A wrapper class that combines a consumer and its input value.
+ *
+ * @param <I> The type of the input value for the consumer.
+ */
 @Value
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class ConsumerWrapper<I> {
@@ -15,10 +20,25 @@ public class ConsumerWrapper<I> {
 
     I input;
 
+    /**
+     * Creates a new ConsumerWrapper instance with the given consumer and input value.
+     *
+     * @param <R>      The type of the input value.
+     * @param consumer The consumer to wrap.
+     * @param input    The input value for the consumer.
+     * @return A new ConsumerWrapper instance.
+     */
     public static <R> ConsumerWrapper<R> of(Consumer<R> consumer, R input) {
         return new ConsumerWrapper<>(consumer, input);
     }
 
+    /**
+     * Creates a function that produces ConsumerWrapper instances with the provided consumer and accepts an input value.
+     *
+     * @param <R>      The type of the input value.
+     * @param consumer The consumer to wrap.
+     * @return A function that produces ConsumerWrapper instances.
+     */
     public static <R> Function<R, ConsumerWrapper<R>> of(Consumer<R> consumer) {
         return input -> ConsumerWrapper.of(consumer, input);
     }
--- src/main/java/jica/spb/async/model/FunctionWrapper.java
+++ src/main/java/jica/spb/async/model/FunctionWrapper.java
@@ -6,6 +6,12 @@ import lombok.Value;
 
 import java.util.function.Function;
 
+/**
+ * A wrapper class that combines a function and its input value.
+ *
+ * @param <I> The type of the input value for the function.
+ * @param <O> The type of the output value produced by the function.
+ */
 @Value
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class FunctionWrapper<I, O> {
@@ -14,10 +20,27 @@ public class FunctionWrapper<I, O> {
 
     I input;
 
+    /**
+     * Creates a new FunctionWrapper instance with the given function and input value.
+     *
+     * @param <R>      The type of the input value.
+     * @param <S>      The type of the output value.
+     * @param function The function to wrap.
+     * @param input    The input value for the function.
+     * @return A new FunctionWrapper instance.
+     */
     public static <R, S> FunctionWrapper<R, S> of(Function<R, S> function, R input) {
         return new FunctionWrapper<>(function, input);
     }
 
+    /**
+     * Creates a function that produces FunctionWrapper instances with the provided function and accepts an input value.
+     *
+     * @param <R>      The type of the input value.
+     * @param <S>      The type of the output value.
+     * @param function The function to wrap.
+     * @return A function that produces FunctionWrapper instances.
+     */
     public static <R, S> Function<R, FunctionWrapper<R, S>> of(Function<R, S> function) {
         return input -> FunctionWrapper.of(function, input);
     }
